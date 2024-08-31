dc310a258ece0173798042323a5dbe4b25d2cc4f
Log97 <87681861+KimHJ97@users.noreply.github.com>
2024-08-31 17:06:42 +0900

    Feat: Swagger Security Item 설정 추가 (#29)
    Swagger 사용시 Security 인증에 의해 모든 API가 실패
    떄문에, Swagger Security 설정을 추가
    
    Ref #28

--- src/main/java/org/example/o2o/config/docs/SwaggerConfig.java
+++ src/main/java/org/example/o2o/config/docs/SwaggerConfig.java
@@ -3,8 +3,11 @@ package org.example.o2o.config.docs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,6 +18,15 @@ public class SwaggerConfig {
 			.info(new Info()
 				.title("O2O Store API")
 				.description("O2O 플랫폼의 스토어 관리 API 문서입니다.")
-				.version("v1.0.0"));
+				.version("v1.0.0"))
+			.addSecurityItem(new SecurityRequirement().addList("token"))
+			.components(new Components()
+				.addSecuritySchemes(
+					"token",
+					new SecurityScheme().type(SecurityScheme.Type.HTTP)
+						.scheme("Bearer")
+						.bearerFormat("JWT")
+				)
+			);
 	}
 }
