3e8d840c578c1412b5dd728a17dcbecbcfcdc542
Jeon Ga Bin <hyanghope@gmail.com>
2024-09-02 22:29:06 +0900

    feat: 음식점 삭제 추가 (#37)

--- src/main/java/org/example/o2o/api/controller/store/StoreController.java
+++ src/main/java/org/example/o2o/api/controller/store/StoreController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,5 +48,14 @@ public class StoreController {
 	public org.example.o2o.common.dto.ApiResponse<StoreDetailResponseDto> getStoreById(@PathVariable Long id) {
 		StoreDetailResponseDto storeDto = storeService.getStoreById(id);
 		return org.example.o2o.common.dto.ApiResponse.success(storeDto);
+	}
+
+	@Operation(summary = "스토어 삭제", description = "스토어 ID를 사용하여 스토어를 삭제합니다.")
+	@ApiResponse(responseCode = "200", description = "성공적으로 스토어를 삭제했습니다.")
+	@ApiResponse(responseCode = "404", description = "스토어를 찾을 수 없습니다.")
+	@DeleteMapping("/{id}")
+	public org.example.o2o.common.dto.ApiResponse<Void> deleteStoreById(@PathVariable Long id) {
+		storeService.deleteStoreById(id);
+		return org.example.o2o.common.dto.ApiResponse.success(null);
 	}
 }
