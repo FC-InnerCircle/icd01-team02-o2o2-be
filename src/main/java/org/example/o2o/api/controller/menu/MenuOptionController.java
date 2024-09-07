ee6a111205cdc3b692ced5e2ba764da0d88cd6ad
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2024-09-08 06:32:56 +0900

    [ISSUE-30][메뉴 옵션] 메뉴 옵션 그룹 관리 (#39)
    * feat: 메뉴 옵션 추가 api
    
    * feat: 메뉴 옵션 삭제 api
    
    * feat: 메뉴 옵션 수정 api
    
    * test: 메뉴 옵션 수정 테스트 추가
    
    * hotfix: fileGroup entity에 menu 의존성 제거

---
+++ src/main/java/org/example/o2o/api/controller/menu/MenuOptionController.java
@@ -0,0 +1,47 @@
+package org.example.o2o.api.controller.menu;
+
+import org.example.o2o.api.docs.menu.MenuOptionDocsController;
+import org.example.o2o.api.dto.menu.request.MenuOptionGroupCreateDto;
+import org.example.o2o.api.dto.menu.response.MenuOptionGroupResponseDto;
+import org.example.o2o.api.service.menu.MenuOptionService;
+import org.example.o2o.common.dto.ApiResponse;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+public class MenuOptionController implements MenuOptionDocsController {
+
+	private final MenuOptionService menuOptionService;
+
+	@PostMapping("/menus/{menuId}/options")
+	public ApiResponse<MenuOptionGroupResponseDto> registerMenuOption(@PathVariable(name = "menuId") Long menuId,
+		@RequestBody MenuOptionGroupCreateDto requestDto) {
+
+		return ApiResponse.success(menuOptionService.register(menuId, requestDto.toStoreMenuOptionGroup()));
+	}
+
+	@DeleteMapping("/options/{optionGroupId}")
+	public ApiResponse<Void> deleteMenuOption(
+		@PathVariable(name = "optionGroupId") Long optionGroupId) {
+
+		menuOptionService.delete(optionGroupId);
+
+		return ApiResponse.success();
+	}
+
+	@PatchMapping("/options/{optionGroupId}")
+	public ApiResponse<MenuOptionGroupResponseDto> updateMenuOption(
+		@PathVariable(name = "optionGroupId") Long optionGroupId, @RequestBody MenuOptionGroupCreateDto requestDto) {
+
+		return ApiResponse.success(menuOptionService.update(optionGroupId, requestDto.toStoreMenuOptionGroup()));
+	}
+}
--- src/main/java/org/example/o2o/config/exception/enums/menu/MenuErrorCode.java
+++ src/main/java/org/example/o2o/config/exception/enums/menu/MenuErrorCode.java
@@ -10,7 +10,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum MenuErrorCode implements ErrorCode {
 
-	NOTFOUND_MENU(HttpStatus.NOT_FOUND.value(), String.valueOf(HttpStatus.NOT_FOUND.value()), "메뉴를 조회할 수 없습니다.");
+	NOTFOUND_MENU(HttpStatus.NOT_FOUND.value(), String.valueOf(HttpStatus.NOT_FOUND.value()), "메뉴를 조회할 수 없습니다."),
+	NOTFOUND_MENU_OPTION(HttpStatus.NOT_FOUND.value(), String.valueOf(HttpStatus.NOT_FOUND.value()),
+		"메뉴 옵션을 조회할 수 없습니다."),
+	REQUIRED_MENU_OPTION(HttpStatus.BAD_REQUEST.value(), String.valueOf(HttpStatus.BAD_REQUEST.value()),
+		"메뉴 옵션은 필수입니다.");
 
 	private final int httpStatusCode;
 	private final String errorCode;
