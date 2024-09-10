b77c5068c588a25625ab3221f16bc9be8e7cda28
chosungil <67699933+sung3441@users.noreply.github.com>
2024-09-10 21:10:03 +0900

    docs: order dto docs 추가

--- src/main/java/org/example/o2o/api/dto/order/request/OrderCreateRequestDto.java
+++ src/main/java/org/example/o2o/api/dto/order/request/OrderCreateRequestDto.java
@@ -8,12 +8,46 @@ import org.example.o2o.domain.order.OrderInfo;
 import org.example.o2o.domain.order.OrderPaymentType;
 import org.example.o2o.domain.order.OrderStatus;
 import org.example.o2o.domain.store.Store;
+import org.hibernate.validator.constraints.Length;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 
 @Builder
-public record OrderCreateRequestDto(String memberId, Long storeId, String storeName, OrderMenuCreateRequestDto[] menus,
-									Integer orderPrice, String payment, Long addressId) {
+public record OrderCreateRequestDto(
+	@Schema(description = "사용자 id", example = "1")
+	@NotNull(message = "사용자 id는 필수입니다.")
+	String memberId,
+
+	@Schema(description = "가게 id", example = "1")
+	@NotNull(message = "가게 id는 필수입니다.")
+	Long storeId,
+
+	@Schema(description = "가게명", example = "김밥천국")
+	@NotNull(message = "가게명은 필수입니다.")
+	String storeName,
+
+	@Schema(description = "메뉴 목록", example = "[]")
+	@NotNull(message = "메뉴 목록은 필수입니다.")
+	@Length(min = 1, message = "메뉴는 하나 이상 선택되어야 합니다.")
+	OrderMenuCreateRequestDto[] menus,
+
+	@Schema(description = "주문 가격", example = "30000")
+	@NotNull(message = "주문 가격은 필수입니다.")
+	@Min(value = 0, message = "주문 가격은 최소 0원입니다.")
+	Integer orderPrice,
+
+	@Schema(description = "결제 수단", example = "card")
+	@NotNull(message = "결제 수단은 필수입니다.")
+	@Pattern(regexp = "^(card)?$", message = "결제 수단은 'card'만 가능합니다.")
+	String payment,
+
+	@Schema(description = "배달 주소 id", example = "1")
+	@NotNull(message = "배달 주소 id는 필수입니다.")
+	Long addressId) {
 
 	public OrderInfo toOrder(Member member, Store store, Address address) {
 		return OrderInfo.builder()
--- src/main/java/org/example/o2o/api/dto/order/request/OrderOptionGroupCreateRequestDto.java
+++ src/main/java/org/example/o2o/api/dto/order/request/OrderOptionGroupCreateRequestDto.java
@@ -1,7 +1,21 @@
 package org.example.o2o.api.dto.order.request;
 
+import org.hibernate.validator.constraints.Length;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
 public record OrderOptionGroupCreateRequestDto(
+	@Schema(description = "메뉴 옵션 그룹 id", example = "1")
+	@NotNull(message = "메뉴 옵션 그룹 id는 필수입니다.")
 	Long optionGroupId,
+
+	@Schema(description = "옵션 그룹명", example = "맵기 조절")
+	@NotNull(message = "옵션 그룹명은 필수입니다.")
 	String optionName,
+
+	@Schema(description = "옵션 항목 목록", example = "[]")
+	@NotNull(message = "옵션 항목은 필수입니다.")
+	@Length(min = 1, message = "옵션 항목은 하나 이상 선택되어야 합니다.")
 	OrderOptionCreateRequestDto[] options) {
 }
